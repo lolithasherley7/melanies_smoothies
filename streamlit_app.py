2026f527fa6dee00f9087101496177efd8c91096
LS <66308441+lolithasherley7@users.noreply.github.com>
2025-06-20 02:01:28 +0530

    Update streamlit_app.py
    Making Use of the SEARCH_ON Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,9 +40,12 @@ if ingredients_list and name_on_order:
     
     for fruit_chosen in ingredients_list:
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write(f"The search value for {fruit_chosen} is {search_on}.")
+        # st.write(f"The search value for {fruit_chosen} is {search_on}.")
         
         st.subheader(f"{fruit_chosen} Nutrition Information")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width =True)
+        
         # Fix the API url - assuming fruit_chosen should be appended, no "watermelon" prefix
         url = f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen.lower().replace(' ', '')}"
         smoothiefroot_response = requests.get(url)
