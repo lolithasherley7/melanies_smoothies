3ece713feaeb4aab2fe924da2b4f11d041c61916
LS <66308441+lolithasherley7@users.noreply.github.com>
2025-06-19 23:28:27 +0530

    Update streamlit_app.py
    Connecting your app to Snowflake is less easy in SniS than it was in SiS
    With SniS, users will be able to connect to your app more easily. You can set up your SniS app in a way that doesn't require them to log in or have a USER in your Snowflake account. In fact, Streamlit will host your app for free if you make it open to the public.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -22,6 +22,7 @@ st.write("The name on the Smoothie will be : ", name_on_order)
 # st.write("Your favourite Fruit is:", option)
 
 
+cnx=st.connection("snowflake")
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
