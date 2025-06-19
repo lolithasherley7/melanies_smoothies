2c99fae1e79d49ec4067a8227add254f1dd6a97b
LS <66308441+lolithasherley7@users.noreply.github.com>
2025-06-20 01:51:47 +0530

    Update streamlit_app.py
     Statement That Will Get Us the "Search On" Value
    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ pd_df= my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
+options = [f"{row['FRUIT_NAME']} ({row['SEARCH_ON']})" for row in pd_df.itertuples()]
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:", 
     [row["FRUIT_NAME"] for row in my_dataframe.collect()],
@@ -48,6 +49,10 @@ if ingredients_list and name_on_order:
     ingredients_string = ' '
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
